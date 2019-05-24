b2d8a3ebfd578054a47da7d75b6253b1d3015edc
dmirizzi <dmirizzi@obsidiansecurity.com>
2019-05-23 21:06:12 -0700

    Add the basic stuff

--- onboarding.sh
+++ onboarding.sh
@@ -4,41 +4,42 @@ Echo "Onboarding..."
 Echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-Echo "Installing iTerm..."
-brew cask install iterm2
-
-Echo "Installing zsh..."
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-Echo "currently using version $(zsh --version | awk '{print $1 $2}') upgrading..."
-upgrade_oh_my_zsh
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+Echo "Moving configs..."
 mv kube-ps1.sh /usr/local/opt/kube-ps1/share/
 mv .kubectl_aliases .zshrc ~/
-Echo "Installing Golang..."
-brew install go 
-mkdir -p ~/go/src/git.obsec.run/backend
-Echo "Installing erlang..."
-brew install erlang
-brew install rebar3
-Echo "Installing Scala..."
-brew install scala
-Echo "Installing Node..."
-brew install node 
-Echo "Installing tmux..."
-brew install tmux
 mv .tmux.conf ~/
-Echo "Installing Docker..."
-brew cask install docker
-pip install docker-compose
-Echo "Installing AWS-CLI..."
-pip install aws
-Echo "Installing aws-login helper"
+
+declare -a taps=()
+declare -a casks=("iterm2" "docekr" "visual-studio-code")
+declare -a beers=("go" "erlang" "rebar3" "scala" "node" "tmux" "kubernetes-cli" "kubernetes-helm" "zsh")
+declare -a pips=("docker-compose" "aws")
+
+for p in "${pips[@]}"
+do
+    echo "installing $p ..."
+    brew install $p
+done
+
 curl -L -o /tmp/aws-login-helper.zip https://git.obsec.run/jturpin/eks-login-helper/-/archive/master/eks-login-helper-master.zip && pip3 install /tmp/aws-login-helper.zip --upgrade
 aws-login-helper.py
-Echo "Installing Kubectl... "
-brew install kubernetes-cli
-Echo "Installing Helm..."
-brew install kubernetes-helm
-Echo "Installing VSCode..."
-brew cask install visual-studio-code
+
+for t in "${taps[@]}"
+do
+    echo "installing $t ..."
+    brew install $t
+done
+
+for c in "${casks[@]}"
+do
+    echo "installing $c ..."
+    brew install $c
+done
+
+for b in "${beers[@]}"
+do
+    echo "installing $b ..."
+    brew install $b
+done
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+upgrade_oh_my_zsh
